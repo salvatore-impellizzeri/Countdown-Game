65603d7679bb1eab4bc5a7d366a2557d98d7aeb3
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2024-12-29 12:50:35 +0100

    Feat/Aggiunto tempo rimasto correttamente

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,6 +1,9 @@
 import { useImperativeHandle, useRef } from "react";
-export default function ResultModal({ref, result, targetTime}) {
+export default function ResultModal({ref, targetTime, remainingTime, onReset}) {
     const dialog = useRef();
+
+    const userLost = remainingTime <= 0;
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
     useImperativeHandle(ref, () => {
         return{
@@ -12,19 +15,17 @@ export default function ResultModal({ref, result, targetTime}) {
 
     return ( 
         <dialog ref={dialog} className="result-modal">
-            <h2>
-                You {result}
-            </h2>
+            { userLost && <h2>You lost</h2>}
             <p>
                 The target time was <strong>{targetTime}</strong>
             </p>
             <p>
-                You stopped the timer with <strong>X seconds left.</strong>
+                You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong>
             </p>
-            <form action="dialog">
+            <form action="dialog" onSubmit={onReset}>
                 <button>
                     Close
-                    </button>
+                </button>
             </form>
         </dialog>
     );
